153101c6ae0be0df074f58f721179419299611b8
0age <37939117+0age@users.noreply.github.com>
2024-10-27 09:14:56 -0700

    implement (totally untested) typehash derivation for deposit + register

---
+++ src/types/ActivatedCompactCategory.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+enum ActivatedCompactCategory {
+    Compact,
+    BatchCompact,
+    MultichainCompact,
+    BatchDepositCompact,
+    BatchDepositBatchCompact,
+    BatchDepositMultichainCompact
+}
--- src/types/CompactCategory.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
-
-enum CompactCategory {
-    Compact,
-    BatchCompact,
-    MultichainCompact
-}
