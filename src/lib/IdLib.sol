580a4e8f6f2fe1dd5fb2a464e6cc1bd0382a904a
0age <37939117+0age@users.noreply.github.com>
2024-11-01 14:33:37 -0700

    add natspec to core interface; small tweaks to metadata renderer and allocator registration fix

--- src/lib/IdLib.sol
+++ src/lib/IdLib.sol
@@ -96,7 +96,7 @@ library IdLib {
     }
 
     function canBeRegistered(address allocator, bytes calldata proof) internal view returns (bool) {
-        return (msg.sender == allocator).or(allocator.code.length > 0).or(proof.length == 86 && (proof[0] == 0xff).and(allocator == address(uint160(uint256(proof.hashCalldata())))));
+        return (msg.sender == allocator).or(allocator.code.length > 0).or(proof.length == 85 && (proof[0] == 0xff).and(allocator == address(uint160(uint256(proof.hashCalldata())))));
     }
 
     function toAllocator(uint256 id) internal view returns (address allocator) {
