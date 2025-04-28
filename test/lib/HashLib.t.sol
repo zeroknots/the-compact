1fbad2b3d41deeb01efe3b513490f4e720310c14
Chris Cashwell <chris.cashwell@uniswap.org>
2025-04-28 14:11:57 -0400

    chore: forge formatting

--- test/lib/HashLib.t.sol
+++ test/lib/HashLib.t.sol
@@ -574,7 +574,8 @@ contract HashLibTester {
         bytes32 typehash,
         bytes32 witness
     ) external returns (bytes32 messageHash) {
-        return HashLib.toFlatMessageHashWithWitness(sponsor, tokenId, amount, arbiter, nonce, expires, typehash, witness);
+        return
+            HashLib.toFlatMessageHashWithWitness(sponsor, tokenId, amount, arbiter, nonce, expires, typehash, witness);
     }
 
     function callToFlatBatchClaimWithWitnessMessageHash(
